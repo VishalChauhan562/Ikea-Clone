9ddc65748f32c160ad30ab3045a611501e525eb6
Vishal Chauhan <86516364+VishalChauhan562@users.noreply.github.com>
2022-03-07 17:49:22 +0530

    Revert "heroku added"

--- my-app/src/components/Pages/HomePage/HomeMain.jsx
+++ my-app/src/components/Pages/HomePage/HomeMain.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Navbar } from '../../../ComponentsB/ProductAll/Navbar';
 import FetchAll from './Fetch/FetchAll';
 import HomePage from "./HomePage";
 
 const HomeMain = () => {
   return (
     <div>
-    <Navbar/>
       <HomePage />
     </div>
   )
